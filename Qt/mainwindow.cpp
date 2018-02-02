91b9d5abfa75f2d3cb65520dd946fdc2dec78b96
Lorenzo Soligo <lorenzo.a7x@gmail.com>
2018-02-02 17:48:49 +0100

    added qt

---
+++ Qt/mainwindow.cpp
@@ -0,0 +1,83 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+    parseRegioni();
+    updateUI();
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::showRegion(QVector<fascia_eta>& qv) {
+    QString fascia, uomini, donne;
+    int rows = qv.size();
+    ui->tableRegion->setRowCount(rows);
+    // int columns = 5;
+    for (int i = 0; i < rows; ++i) {
+        ui->tableRegion->setItem(i, 0, new QTableWidgetItem(QString::fromStdString(qv[i].fascia)));
+        ui->tableRegion->setItem(i, 1, new QTableWidgetItem(QString::number(qv[i].uomini)));
+        ui->tableRegion->setItem(i, 2, new QTableWidgetItem(QString::number(qv[i].pcUomini, 'f', 2)));
+        ui->tableRegion->setItem(i, 3, new QTableWidgetItem(QString::number(qv[i].donne)));
+        ui->tableRegion->setItem(i, 4, new QTableWidgetItem(QString::number(qv[i].pcDonne, 'f', 2)));
+
+    }
+}
+
+void MainWindow::updateUI() {
+    ui->tableRegion->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    QList<QString> headers;
+    headers.append("Fascia");
+    headers.append("Uomini");
+    headers.append("% Uomini");
+    headers.append("Donne");
+    headers.append("% Donne");
+    QStringList *qHeaders = new QStringList(headers);
+    ui->tableRegion->setHorizontalHeaderLabels(*qHeaders);
+    ui->tableRegion->verticalHeader()->hide();
+
+    QMap<std::string, QVector<fascia_eta> >::iterator i, ie;
+    for(i = regioniFasce.begin(), ie = regioniFasce.end(); i != ie; ++i) {
+        ui->cbRegion->addItem(QString::fromStdString(i.key()));
+    }
+}
+
+void MainWindow::parseRegioni() {
+    QString filename = "./data.txt";
+    std::ifstream is;
+    is.open(filename.toStdString().c_str());
+    std::string regione, fascia;
+    int uomini, donne;
+
+    if(is.is_open()) {
+        is >> regione;
+        while (regione != "<END>") {
+            is >> fascia >> uomini >> donne;
+            double xx = 100 * static_cast<double>(donne) / (uomini + donne);
+            double xy = 100 * static_cast<double>(uomini) / (uomini + donne);
+            if (regioniFasce.count(regione) == 0) {
+                QVector<fascia_eta> qv;
+                regioniFasce.insert(regione, qv);
+                regioniFasce[regione].push_back(fascia_eta(fascia, uomini, xy, donne, xx));
+            }
+            else {
+                regioniFasce[regione].push_back(fascia_eta(fascia, uomini, xy, donne, xx));
+            }
+            is >> regione;
+        }
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, const fascia_eta &f) {
+    os << f.fascia << " " << f.uomini << " " << f.donne;
+    return os;
+}
+
+void MainWindow::on_cbRegion_currentTextChanged(const QString &arg1) {
+    showRegion(regioniFasce[arg1.toStdString()]);
+    ui->labRegion->setText(arg1);
+}
---
+++ Qt/mainwindow.h
@@ -0,0 +1,54 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtWidgets>
+#include <iostream>
+#include <string>
+#include <QString>
+#include <QMap>
+#include <QVector>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+namespace Ui {
+class MainWindow;
+}
+
+struct fascia_eta {
+    std::string fascia;
+    int uomini;
+    double pcUomini;
+    int donne;
+    double pcDonne;
+    fascia_eta() {}
+    fascia_eta(std::string &f, int u, double pu, int d, double pd) : fascia(f), uomini(u), pcUomini(pu), donne(d), pcDonne(pd) {}
+};
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void showRegion(QVector<fascia_eta>& qv);
+    QMap<std::string, QVector<fascia_eta> > regioniFasce;
+    void selectRegione(std::string &s);
+    void parseRegioni();
+
+
+private slots:
+    void on_cbRegion_currentTextChanged(const QString &arg1);
+
+private:
+    Ui::MainWindow *ui;
+    void updateUI();
+};
+
+
+
+std::ostream &operator<<(std::ostream &os, const fascia_eta &f);
+
+
+#endif // MAINWINDOW_H
